85429bfdc95a0f51b50bfa273cf95a280f9c11a9
sanyamsinghai <144606901+sanyamsinghai@users.noreply.github.com>
2024-04-10 22:48:09 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Missing number/missing-number.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int missingNumber(int n, vector<int> &arr) {
+        // code here
+        int sum=n*(n+1)/2;
+        
+        int s=0;
+        
+        for(int i=0 ; i<n-1 ; i++){
+            s+=arr[i];
+        }
+        
+        return (sum-s);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> arr(n-1);
+        Array::input(arr,n-1);
+        
+        Solution obj;
+        int res = obj.missingNumber(n, arr);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
