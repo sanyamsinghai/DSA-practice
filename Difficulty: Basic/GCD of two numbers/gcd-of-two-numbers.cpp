fd94dd23737d5c7c93aa1f13f9a5619af12e4823
sanyamsinghai <144606901+sanyamsinghai@users.noreply.github.com>
2025-03-08 22:05:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    int gcd(int a, int b) {
+        while(a>0 && b>0){
+            if(a>b){
+                a=a%b;
+            }
+            else
+            b=b%a;
+        }
+        if(a==0) return b;
+        return a;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int a;
+        scanf("%d",&a);
+        
+        
+        int b;
+        scanf("%d",&b);
+        
+        Solution obj;
+        int res = obj.gcd(a, b);
+        
+        cout<<res<<endl;
+        
+    
+cout << "~" << "\n";
+}
+}
+
+// } Driver Code Ends
