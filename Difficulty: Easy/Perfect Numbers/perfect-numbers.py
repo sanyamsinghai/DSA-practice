cce94bd7ad579936a923320d05c4c48fdbf8ef9b
sanyamsinghai <144606901+sanyamsinghai@users.noreply.github.com>
2026-07-01 18:03:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Perfect Numbers/perfect-numbers.py
@@ -0,0 +1,18 @@
+import math
+class Solution:
+    def isPerfect(self, num):
+        # code here 
+        if num <= 1:
+            return False
+        
+        sum_divisors = 1 
+        sqrt_num = int(math.sqrt(num))
+        
+        for i in range(2, sqrt_num + 1):
+            if num % i == 0:
+                sum_divisors += i
+                paired = num // i
+                if paired != i: 
+                    sum_divisors += paired
+        
+        return sum_divisors == num
