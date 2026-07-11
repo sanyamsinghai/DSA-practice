50ab6fa20f2ce8c807d579d9918de81562eb8bc3
sanyamsinghai <144606901+sanyamsinghai@users.noreply.github.com>
2026-07-11 20:00:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (84.24%) - LeetSync

---
+++ 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def evenNumberBitwiseORs(self, nums: List[int]) -> int:
+        ans = 0
+        for i in nums:
+            if i % 2 == 0:
+                ans |= i
+
+        return ans
