4e3983df2c6a85dde3b824aedfe29745976de6e1
sanyamsinghai <144606901+sanyamsinghai@users.noreply.github.com>
2026-07-13 15:36:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxWater(self, height):
+        # code here
+        i = 0
+        j = len(height) - 1
+        maxWater = 0
+
+        while i < j:
+            width = j - i
+            area = min(height[i], height[j]) * width
+            maxWater = max(maxWater, area)
+
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+        return maxWater
