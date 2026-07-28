a1cd1650df87e4dcb5fbc12a1e3ffb31673ce8fc
sanyamsinghai <144606901+sanyamsinghai@users.noreply.github.com>
2026-07-28 11:37:05 +0530

    Time: 208 ms (65.90%) | Memory: 20.8 MB (65.44%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -1,5 +1,25 @@
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
+        # d = {}
+        # ans = ""
+        # for i in range(len(s) // 2):
+        #     if s[i] in d:
+        #         d[s[i]] += 1
+        #     else:
+        #         d[s[i]] = 1
+
+        # temp = ""
+        # for ch in sorted(d):
+        #     temp += ch * d[ch]
+        # temp2 = temp[::-1]
+
+        # if len(s) % 2 == 0:
+        #     ans = temp + temp2
+        # else:
+        #     ans = temp + s[len(s) // 2] + temp2
+
+        # return ans
+
         d = {}
         ans = ""
         for i in range(len(s) // 2):
@@ -7,15 +27,17 @@ class Solution:
                 d[s[i]] += 1
             else:
                 d[s[i]] = 1
+                
+        temp = []
+        for i in range(26):
+            ch = chr(ord("a") + i)
+            if ch in d:
+                temp.append(ch * d[ch])
 
-        temp = ""
-        for ch in sorted(d):
-            temp += ch * d[ch]
-        temp2 = temp[::-1]
-
+        left = "".join(temp)
         if len(s) % 2 == 0:
-            ans = temp + temp2
+            ans = left + left[::-1]
         else:
-            ans = temp + s[len(s) // 2] + temp2
+            ans = left + s[len(s) // 2] + left[::-1]
 
         return ans
