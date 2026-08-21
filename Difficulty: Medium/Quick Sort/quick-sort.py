f6e4f99ec2beaa66d9f4e941449dc148ba99d8d5
sanyamsinghai <144606901+sanyamsinghai@users.noreply.github.com>
2026-08-21 19:48:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Quick Sort/quick-sort.py
@@ -0,0 +1,22 @@
+class Solution:
+    def quickSort(self, arr, low, high):
+        # code here 
+        if low < high:
+            p = self.partition(arr, low, high)
+
+            self.quickSort(arr, low, p - 1)
+            self.quickSort(arr, p + 1, high)
+
+    def partition(self, arr, low, high):
+        # code here
+        pivot = arr[high]
+        i = low - 1
+
+        for j in range(low, high):
+            if arr[j] <= pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+
+        arr[i + 1], arr[high] = arr[high], arr[i + 1]
+
+        return i + 1
